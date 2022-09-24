b6de8bc1429fdf5b7ce1603e69930138d47b77f4
Rainforest <rainforestnick@gmail.com>
2022-09-24 04:46:25 +0000

    feat: project init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Canvas } from '@react-three/fiber';
+import Box from './components/box';
+
+function App() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+}
+
+export default App;
---
+++ src/components/box.tsx
@@ -0,0 +1,29 @@
+import * as THREE from 'three';
+import { ThreeElements, useFrame } from '@react-three/fiber';
+import { useRef, useState } from 'react';
+
+const Box = (props: ThreeElements['mesh']) => {
+  const mesh = useRef<THREE.Mesh>(null);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => {
+    console.log(state, delta);
+    mesh.current?.rotation.x && (mesh.current.rotation.x += 0.01);
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+};
+
+export default Box;
