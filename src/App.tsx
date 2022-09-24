8bc1876bf104d6eaf82e2447f9d31b40a1def1bc
Rainforest <rainforestnick@gmail.com>
2022-09-24 10:48:06 +0000

    feat: add spotlight route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from '~/pages';
 import Threejs from '~/layouts/threejs';
+import Home from '~/pages';
 import Mountains from '~/pages/mountains';
+import Spotlight from './pages/spotlight';
 
 const App = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path='threejs' element={<Threejs />}>
+        <Route path='spotlight' element={<Spotlight />} />
         <Route path='mountains' element={<Mountains />} />
       </Route>
     </Routes>
---
+++ src/pages/spotlight.tsx
@@ -0,0 +1,5 @@
+const Spotlight = () => {
+  return <></>;
+};
+
+export default Spotlight;
